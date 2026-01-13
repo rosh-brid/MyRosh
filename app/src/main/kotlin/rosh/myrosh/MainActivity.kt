2936f37ab800b121c26b6f5df15fa5a538996013
rosh-brid <roshbrid@gmail.com>
2026-01-13 08:11:37 +0700

    Create MainActivity.kt

---
+++ app/src/main/kotlin/rosh/myrosh/MainActivity.kt
@@ -0,0 +1,12 @@
+package rosh.myrosh
+
+import android.widget.*
+import android.os.*
+import androidx.appcompat.app.*
+
+class MainActivity : AppCompatActivity(){
+  override fun onCreate(savedInstanceState:Bundle?){
+    super.onCreate(savedInstanceState)
+    
+  }
+}
