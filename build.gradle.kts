656607bdf8bdbeed847a0a94be54da6964ec853a
Rosh <roshbrid@gmail.com>
2026-03-20 19:47:02 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("com.android.library") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false     
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false   
+	id("com.google.gms.google-services") version "4.4.4" apply false  
 }
 
 tasks.register<Delete>("clean") {
