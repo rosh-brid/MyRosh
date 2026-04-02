e8232cac8986618589a06afdc3f8262ffde75047
Rosh <roshbrid@gmail.com>
2026-04-02 08:36:25 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,33 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "rosh.myrosh"
     compileSdk = 34
+    ndkVersion = "29.0.14033849"
 
     defaultConfig {
         applicationId = "rosh.myrosh"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.2"
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+	ndk {
+            abiFilters.add("arm64-v8a")
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "4.1.2"
+        }
     }
 
     buildFeatures {
@@ -43,5 +56,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.activity:activity:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
     implementation("com.google.android.gms:play-services-ads:23.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("io.github.Rosemoe.sora-editor:editor:0.23.0")
+    implementation("io.github.Rosemoe.sora-editor:language-java:0.23.0")
+    implementation("io.github.Rosemoe.sora-editor:editor-lsp:0.23.0")
 }
